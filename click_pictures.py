84c166fcaa84305ccbe55eacc5c3b14a6d1d9f93
Varada Harikumar <42372100+TheEdelweissPirate@users.noreply.github.com>
2019-12-17 14:32:25 +0530

    To save those 5 photos in a single folder.
    To be run on command line only. The folder name will be the ID number of the student.

--- click_pictures.py
+++ click_pictures.py
@@ -1,19 +1,34 @@
 import cv2
-
+import os
+import os.path
+import argparse
 cam = cv2.VideoCapture(0)
 cv2.namedWindow("Capture")
 
+ap = argparse.ArgumentParser()
+#ap.add_argument("-w","--workdir",required=True,help="The path to your working directory")
+ap.add_argument("-i","--ID",required=True,help="The identification number of student")
+args=vars(ap.parse_args())
+
 print ("Press the space key to capture")
 
+
 img_counter=0
+#ID = raw_input("Enter the student ID")
+#savePath=args["workdir"]
+savePath= "C:\Users\Hari kumar\Desktop\PHASE01"
+os.mkdir(os.path.join(savePath,args["ID"]))
 while True:
+       
        if img_counter<5:
+              
               k= cv2.waitKey(1)
               ret,frame = cam.read()
               cv2.imshow("Capture",frame)
               if k%256 == 32:
-                     img_name="image {}.png".format(img_counter)
-                     cv2.imwrite(img_name,frame)
+                     
+                     fileName = os.path.join(savePath,args["ID"],"IMAGE "+str(img_counter)+".png")
+                     cv2.imwrite(fileName,frame)
                      img_counter+=1
                      print "Image captured"
        else:
@@ -23,3 +38,4 @@ while True:
 
 cam.release()
 cv2.destroyAllWindows()
+ 
